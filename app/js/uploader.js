1ff8a2c75ac357a96cf41153585b7558e20ef265
Vyacheslav <vyacheslav@book.home>
2018-03-12 03:49:53 +1100

    добавил прогресбар загрузки и индикацию стадии выполнения загрузки и соединения частей файла.

--- app/js/uploader.js
+++ app/js/uploader.js
@@ -1,8 +1,8 @@
-function send_part(part_num, part_size, step_size, step, file, result_array, task_id) {
+
+function send_part(part_num, part_size, step_size, step, file, threads_stop_array, task_id, parts_upload_array) {
   if(step >= part_num){
-    console.log("upload step: " + step + " done");
     console.log("upload thread №" + (step % step_size) + " done");
-    result_array[step % step_size] = true;
+    threads_stop_array[step % step_size] = true;
   }
   else{
     console.log("part num: " + part_num + " step_size: " + step_size + " step: " + step + ".");
@@ -18,22 +18,36 @@ function send_part(part_num, part_size, step_size, step, file, result_array, tas
         var url = JSON.parse(this.responseText).UploadURL;
         reader.readAsArrayBuffer(file.slice(step*part_size, (step + 1)*part_size));
         reader.onloadend = function () {
-          var sender = new XMLHttpRequest();
-          sender.open("POST", url);
           var form = new FormData();
           form.append("file", new Blob([this.result]));
           form.append("file_name", file.name);
           form.append("part_num", step);
           form.append("task_id", task_id);
-          sender.send(form);
+          var sender = new XMLHttpRequest();
+          //sender.addEventListener("progress", function (oEvent) {
+          //  console.log("progress");
+          //  console.log(oEvent);
+          //  if (oEvent.lengthComputable) {
+          //    parts_upload_array[step] = oEvent.loaded / oEvent.total;
+          //    console.log("step: " + step + " load: " + (oEvent.loaded / oEvent.total));
+          //  }
+          //});
+          sender.upload.onprogress = function(pe) {
+            if (pe.lengthComputable) {
+              parts_upload_array[step] = pe.loaded / pe.total;
+            }
+          };
+          sender.open("POST", url);
           sender.onload = function () {
             if(this.status != 200){
               console.log("error file part upload. status: " + this.status);
             }
             else{
-              send_part(part_num, part_size, step_size, step + step_size, file, result_array, task_id);
+              parts_upload_array[step] = 1;
+              send_part(part_num, part_size, step_size, step + step_size, file, threads_stop_array, task_id, parts_upload_array);
             }
           };
+          sender.send(form);
         };
       }
     };
@@ -50,7 +64,7 @@ function makeid() {
   return text;
 }
 
-function get_download_url(task_id) {
+function get_download_url(task_id, stage) {
   var sender = new XMLHttpRequest();
   sender.open("GET", "/download_url/" + task_id);
   sender.send();
@@ -60,12 +74,13 @@ function get_download_url(task_id) {
     }
     else{
       var DownloadURL = JSON.parse(this.responseText).DownloadURL;
+      stage.href = DownloadURL;
       console.log("download_url: " + DownloadURL);
     }
   }
 }
 
-function wait_end_merge(task_id) {
+function wait_end_merge(task_id, stage) {
   var sender = new XMLHttpRequest();
   sender.open("GET", "/merge_is_done/" + task_id);
   sender.send();
@@ -78,21 +93,22 @@ function wait_end_merge(task_id) {
       console.log("is done: " + isDone);
       if(isDone) {
         console.log("merge done.");
-        get_download_url(task_id);
+        get_download_url(task_id, stage);
+        stage.text = "Done";
       }
       else{
         console.log("wait");
-        setTimeout(function() {wait_end_merge(task_id)}, 1000);
+        setTimeout(function() {wait_end_merge(task_id, stage)}, 2000);
       }
     }
   }
 }
 
-function merge_parts(result_array, file_name, parts, task_id) {
-  var is_end = result_array.every(function (element, index, array) {
+function merge_parts(threads_stop_array, file_name, parts, task_id, stage) {
+  var is_end = threads_stop_array.every(function (element, index, array) {
     return element;
   });
-  if (!is_end) setTimeout(merge_parts, 1000, result_array, file_name, parts, task_id);
+  if (!is_end) setTimeout(merge_parts, 2000, threads_stop_array, file_name, parts, task_id, stage);
   else{
     console.log("all thereads stop");
     var sender = new XMLHttpRequest();
@@ -109,27 +125,45 @@ function merge_parts(result_array, file_name, parts, task_id) {
       else{
         console.log("merge task runing, task_id: " + task_id);
         //merge begin, wait_end
-        wait_end_merge(task_id);
+        wait_end_merge(task_id, stage);
       };
     }
   }
 }
 
+function show_progress(parts_upload_array, parts, progress_bar, stage) {
+  var progress = parts_upload_array.reduce(function (a, b) {return a+b;}, 0) / parts * 1;
+  progress_bar.value = progress;
+  if (progress < 1) setTimeout(show_progress, 1000, parts_upload_array, parts, progress_bar, stage);
+  else stage.text = "Merging";
+}
+
 function handleSubmit(e) {
-  const part_size = 1024*1024*256; //256MB
+  const part_size = 1024*1024*256 ; //256MB
   const nthreads = 2;
-  const result_array = [];
+  const threads_stop_array = []; //для отслеживания завершившихся потоков
+  const parts_upload_array = []; //для отслеживания завершённых загрузок
+  var progress_bar = document.getElementById("progress-bar");
+  var stage = document.getElementById("stage");
+  progress_bar.value = 0;
+  stage.text = "Upload";
+  stage.href = "#";
   e.preventDefault();
   if(this.file.files[0] === undefined) return false;
   var file_ = this.file.files[0];
   const parts = Math.ceil(file_.size / part_size);
   for(var i=0; i < nthreads; i++) {
-    result_array[i] = false;
+    if (i < parts) threads_stop_array[i] = false;
+    else threads_stop_array[i] = true;
+  }
+  for(var i=0; i < parts; i++){
+    parts_upload_array[i] = 0;
   }
   var task_id = makeid();
   for(var i=0; i < nthreads; i++){
-    send_part(parts, part_size, nthreads, i, file_, result_array, task_id);
+    if (i < parts) send_part(parts, part_size, nthreads, i, file_, threads_stop_array, task_id, parts_upload_array);
   }
-  merge_parts(result_array, file_.name, parts, task_id);
+  show_progress(parts_upload_array, parts, progress_bar, stage);
+  merge_parts(threads_stop_array, file_.name, parts, task_id, stage);
   return false;
 }
