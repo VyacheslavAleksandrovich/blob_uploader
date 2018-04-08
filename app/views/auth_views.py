18ab2113879aaf9cd2341bf47ba20db474150902
Vyacheslav <vyacheslav@book.home>
2018-04-08 14:13:31 +1100

    ограничил регистрацию для определённых пользователей

--- app/views/auth_views.py
+++ app/views/auth_views.py
@@ -20,31 +20,33 @@ class SignupHandler(BaseHandler):
         last_name = self.request.get('lastname')
 
         unique_properties = ['email_address']
-        user_data = self.user_model.create_user(user_name,
-                                                unique_properties,
-                                                email_address=email,
-                                                name=name,
-                                                password_raw=password,
-                                                last_name=last_name,
-                                                verified=False)
-        if not user_data[0]: #user_data is a tuple
-            self.display_message(
-                'Unable to create user for email %s because of duplicate keys %s' % (user_name, user_data[1]))
-            return
+        allowed_user = email in ['anastasia.97@mail.ru', 'artyom@proteiq.com', 'maneron@gmail.com', 'ruslan.tetuev@gmail.com']
+        if allowed_user:
+            user_data = self.user_model.create_user(user_name,
+                                                    unique_properties,
+                                                    email_address=email,
+                                                    name=name,
+                                                    password_raw=password,
+                                                    last_name=last_name,
+                                                    verified=False)
+            if not user_data[0]: #user_data is a tuple
+                self.display_message(
+                    'Unable to create user for email %s because of duplicate keys %s' % (user_name, user_data[1]))
+                return
 
-        user = user_data[1]
-        user_id = user.get_id()
+            user = user_data[1]
+            user_id = user.get_id()
 
-        token = self.user_model.create_signup_token(user_id)
-        verification_url = self.uri_for('verification',
-                                        type='v',
-                                        user_id=user_id,
-                                        signup_token=token,
-                                        _full=True)
-        #msg = 'Send an email to user in order to verify their address. \
-        #      They will be able to do so by visiting <a href="{url}">{url}</a>'
-        #
-        #self.display_message(msg.format(url=verification_url))
+            token = self.user_model.create_signup_token(user_id)
+            verification_url = self.uri_for('verification',
+                                            type='v',
+                                            user_id=user_id,
+                                            signup_token=token,
+                                            _full=True)
+            #msg = 'Send an email to user in order to verify their address. \
+            #      They will be able to do so by visiting <a href="{url}">{url}</a>'
+            #
+            #self.display_message(msg.format(url=verification_url))
         self.redirect(self.uri_for('home'))
 
 
